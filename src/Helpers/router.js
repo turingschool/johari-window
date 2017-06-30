e8d11ab02caf75be8ad99939a41ac574db0e6923
Mike Schutte <tmikeschutte@gmail.com>
2017-06-30 10:36:41 -0600

    Add AdminRoute
    Refactor login to pure component

--- src/Helpers/router.js
+++ src/Helpers/router.js
@@ -5,15 +5,39 @@ export const PrivateRoute = ({ auth, component: Component, user, ...rest }) => (
   <Route
     {...rest}
     render={props => (
-      auth.loggedIn()
-        ? (<Component user={user} {...rest}/>)
-        : (<Redirect
-              to={{
-                pathname: '/login',
-                state: { from: props.location }
-              }}
-            />
-          )
+      auth.loggedIn() ? (
+        <Component user={user} {...rest}/>
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+            state: { from: props.location }
+          }}
+        />
+      )
     )}
+  />
+)
+
+export const AdminRoute = ({
+  auth, component: Component, user, ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props => {
+      const cohortID = { cohortID: props.match.params.id }
+      return (
+        auth.loggedIn() && user.role === "staff" ? (
+          <Component user={user} {...rest} {...cohortID} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/',
+              state: { from: props.location }
+            }}
+          />
+        )
+      )
+    }}
   />
 )
